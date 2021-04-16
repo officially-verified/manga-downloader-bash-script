db23bd5766434e7ef5aecfadbb7cabc4a71c5151
officially-verified <30338664+officially-verified@users.noreply.github.com>
2021-04-17 05:45:29 +0800

    Update mangadownloader.sh

--- mangadownloader.sh
+++ mangadownloader.sh
@@ -17,17 +17,18 @@ initialize() {
 
 download_chapter() {
     local chapter_no=$1
-    local file_downloaded=$MANGA\_$chapter_no
+    local file_downloaded=$EXTRACT_DIRECTORY\_$chapter_no
     local chapter_download_url=$SITE_SOURCE$file_downloaded
 
-    wget -q --show-progress $chapter_download_url 
+    wget -q --show-progress $chapter_download_url
 
     local file_mimetype=$(file -bi $file_downloaded)
 
     if [[ $file_mimetype == "application/zip"* ]];
     then
-        local extract_destination=$EXTRACT_DIRECTORY/CHAPTER_$(printf "%03d" $chapter_no)
+        local extract_destination=$EXTRACT_DIRECTORY/CHAPTER_$(printf "%04d" $chapter_no)
         unzip -qd $extract_destination $file_downloaded
+        rename_file_with_leading_zeroes $extract_destination
         rm $file_downloaded
     else
         echo "Not a zip file"
@@ -41,6 +42,16 @@ download() {
     for (( chapter_no = $CHAPTER_START; chapter_no <= $CHAPTER_END; chapter_no++ ))
     do
         download_chapter $chapter_no
+    done
+}
+
+rename_file_with_leading_zeroes() {
+    local chapter_folder=$1
+
+    for file in $chapter_folder/*;
+    do
+        local file_no=$(echo $file | grep -oP "\d+(?=.jpg)")
+        mv $file $chapter_folder/$(printf "%02d.jpg" $file_no)
     done
 }
 
